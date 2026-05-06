e556975430d8870fe99a3023d2146f957aa80151
ayla-imran851 <ayla@macworldtechnology.com>
2026-05-06 11:51:48 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // 🔥 IMPORTANT FIX
+  turbopack: {},
+
+  // force stable bundling behavior
+  webpack: (config) => {
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
